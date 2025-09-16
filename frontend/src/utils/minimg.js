2bab5571084165a172841ffbfcb36a0ac3c50df8
Alex <alex@MacBook-Pro-Alex.local>
2025-09-16 16:45:16 +0300

    16/09/2025

--- frontend/src/utils/minimg.js
+++ frontend/src/utils/minimg.js
@@ -1,20 +1,23 @@
+import { getThumbnailPath } from "./thumb";
 const API_URL = process.env.REACT_APP_API_URL.replace('/api', '');
+
+
 export function minimg(product) {
 console.log('product',product)
 
   if (product?.images && product.images.length > 0) {
     const featured = product.images.find((img) => img.is_featured_image);
     return featured
-      ? `${API_URL}${featured.image_path}`
-      : `${API_URL}${product.images[0].image_path}`;
+      ? `${API_URL}${getThumbnailPath(featured.image_path)}`
+      : `${API_URL}${getThumbnailPath(product.images[0].image_path)}`;
   }
 
   // ДОБАВЛЕНО: если нет индивидуальных — использовать model_images
   if (product?.model_images && product.model_images.length > 0) {
     const featured = product.model_images.find((img) => img.is_featured_image);
     return featured
-      ? `${API_URL}${featured.image_path}`
-      : `${API_URL}${product.model_images[0].image_path}`;
+      ? `${API_URL}${getThumbnailPath(featured.image_path)}`
+      : `${API_URL}${getThumbnailPath(product.model_images[0].image_path)}`;
   }
 
   return "https://via.placeholder.com/220x220";
